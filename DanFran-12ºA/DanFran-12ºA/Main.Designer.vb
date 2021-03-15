bca1df0225eaa6994bd883848f76ab51d8535202
dan-panela <80076408+dan-panela@users.noreply.github.com>
2021-03-15 23:10:18 +0000

    main

--- DanFran-12ºA/DanFran-12ºA/Main.Designer.vb
+++ DanFran-12ºA/DanFran-12ºA/Main.Designer.vb
@@ -31,12 +31,12 @@ Partial Class Main
         '
         'Label1
         '
-        Me.Label1.BackColor = System.Drawing.Color.RosyBrown
+        Me.Label1.BackColor = System.Drawing.Color.MediumAquamarine
         Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label1.Location = New System.Drawing.Point(2, 258)
+        Me.Label1.Location = New System.Drawing.Point(-2, 304)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(789, 119)
+        Me.Label1.Size = New System.Drawing.Size(875, 72)
         Me.Label1.TabIndex = 0
         '
         'BJogar
@@ -44,9 +44,9 @@ Partial Class Main
         Me.BJogar.BackColor = System.Drawing.Color.MistyRose
         Me.BJogar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.BJogar.Font = New System.Drawing.Font("Gadugi", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BJogar.Location = New System.Drawing.Point(627, 282)
+        Me.BJogar.Location = New System.Drawing.Point(700, 314)
         Me.BJogar.Name = "BJogar"
-        Me.BJogar.Size = New System.Drawing.Size(151, 72)
+        Me.BJogar.Size = New System.Drawing.Size(128, 45)
         Me.BJogar.TabIndex = 1
         Me.BJogar.Text = "Jogar"
         Me.BJogar.UseVisualStyleBackColor = False
@@ -56,9 +56,9 @@ Partial Class Main
         Me.Bregras.BackColor = System.Drawing.Color.MistyRose
         Me.Bregras.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Bregras.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Bregras.Location = New System.Drawing.Point(512, 311)
+        Me.Bregras.Location = New System.Drawing.Point(549, 314)
         Me.Bregras.Name = "Bregras"
-        Me.Bregras.Size = New System.Drawing.Size(109, 43)
+        Me.Bregras.Size = New System.Drawing.Size(124, 45)
         Me.Bregras.TabIndex = 2
         Me.Bregras.Text = "Regras"
         Me.Bregras.UseVisualStyleBackColor = False
@@ -68,7 +68,7 @@ Partial Class Main
         Me.BSair.BackColor = System.Drawing.Color.MistyRose
         Me.BSair.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.BSair.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BSair.Location = New System.Drawing.Point(12, 317)
+        Me.BSair.Location = New System.Drawing.Point(26, 322)
         Me.BSair.Name = "BSair"
         Me.BSair.Size = New System.Drawing.Size(109, 37)
         Me.BSair.TabIndex = 3
@@ -77,7 +77,7 @@ Partial Class Main
         '
         'MonthCalendar1
         '
-        Me.MonthCalendar1.Location = New System.Drawing.Point(12, 18)
+        Me.MonthCalendar1.Location = New System.Drawing.Point(35, 42)
         Me.MonthCalendar1.Name = "MonthCalendar1"
         Me.MonthCalendar1.TabIndex = 4
         Me.MonthCalendar1.TitleBackColor = System.Drawing.Color.LightSeaGreen
@@ -86,7 +86,9 @@ Partial Class Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(790, 377)
+        Me.BackgroundImage = Global.DanFran_12ºA.My.Resources.Resources.maindef
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(871, 374)
         Me.Controls.Add(Me.MonthCalendar1)
         Me.Controls.Add(Me.BSair)
         Me.Controls.Add(Me.Bregras)
--- DanFran-12ºA/DanFran-12ºA/My Project/Resources.Designer.vb
+++ DanFran-12ºA/DanFran-12ºA/My Project/Resources.Designer.vb
@@ -133,6 +133,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property maindef() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("maindef", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property mapadef() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("mapadef", resourceCulture)
