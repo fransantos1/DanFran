1e69d4a1fe9d88feb95cf31bc142d030d20771c9
Xchicko <49843015+fransantos1@users.noreply.github.com>
2021-03-24 11:25:19 +0000

    merda do filipe

--- DanFran-12ºA/DanFran-12ºA/Mundo2.vb
+++ DanFran-12ºA/DanFran-12ºA/Mundo2.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Mundo2
-
+    Dim ganhar As Boolean = True
     Public Sub New()
 
         ' This call is required by the designer.
@@ -28,8 +28,15 @@
 
 
     Private Sub PBExit_Click(sender As Object, e As EventArgs) Handles PBExit.Click
-        Me.Hide()
-        Fim.Show()
+        If ganhar = True Then
+            Me.Hide()
+            Fim.Show()
+        Else
+            MsgBox("Não faças batota, tenta outra vez.", MsgBoxStyle.Information, "):")
+            MoveToStart()
+            ganhar = True
+        End If
+
     End Sub
 
     Private Sub Wall_MouseEnter(sender As Object, e As EventArgs) Handles Label11.MouseEnter, Label92.MouseEnter, Label91.MouseEnter, Label90.MouseEnter, Label9.MouseEnter, Label89.MouseEnter, Label88.MouseEnter, Label87.MouseEnter, Label86.MouseEnter, Label85.MouseEnter, Label84.MouseEnter, Label83.MouseEnter, Label82.MouseEnter, Label81.MouseEnter, Label80.MouseEnter, Label8.MouseEnter, Label79.MouseEnter, Label78.MouseEnter, Label77.MouseEnter, Label76.MouseEnter, Label75.MouseEnter, Label74.MouseEnter, Label73.MouseEnter, Label72.MouseEnter, Label71.MouseEnter, Label70.MouseEnter, Label7.MouseEnter, Label69.MouseEnter, Label68.MouseEnter, Label67.MouseEnter, Label66.MouseEnter, Label65.MouseEnter, Label64.MouseEnter, Label63.MouseEnter, Label62.MouseEnter, Label61.MouseEnter, Label60.MouseEnter, Label6.MouseEnter, Label59.MouseEnter, Label58.MouseEnter, Label57.MouseEnter, Label55.MouseEnter, Label54.MouseEnter, Label53.MouseEnter, Label52.MouseEnter, Label51.MouseEnter, Label50.MouseEnter, Label5.MouseEnter, Label49.MouseEnter, Label48.MouseEnter, Label47.MouseEnter, Label46.MouseEnter, Label45.MouseEnter, Label44.MouseEnter, Label43.MouseEnter, Label42.MouseEnter, Label41.MouseEnter, Label40.MouseEnter, Label4.MouseEnter, Label39.MouseEnter, Label38.MouseEnter, Label37.MouseEnter, Label36.MouseEnter, Label35.MouseEnter, Label34.MouseEnter, Label33.MouseEnter, Label32.MouseEnter, Label31.MouseEnter, Label30.MouseEnter, Label3.MouseEnter, Label29.MouseEnter, Label28.MouseEnter, Label27.MouseEnter, Label26.MouseEnter, Label25.MouseEnter, Label24.MouseEnter, Label23.MouseEnter, Label22.MouseEnter, Label21.MouseEnter, Label20.MouseEnter, Label2.MouseEnter, Label19.MouseEnter, Label18.MouseEnter, Label17.MouseEnter, Label16.MouseEnter, Label15.MouseEnter, Label14.MouseEnter, Label13.MouseEnter, Label12.MouseEnter, Label1.MouseEnter
@@ -40,5 +47,9 @@
         picmino.Visible = False
         lblmino.Visible = False
         PicFala.Visible = False
+    End Sub
+
+    Private Sub Mundo2_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ganhar = False
     End Sub
 End Class
--- DanFran-12ºA/DanFran-12ºA/Pok-Man.vb
+++ DanFran-12ºA/DanFran-12ºA/Pok-Man.vb
@@ -3,6 +3,8 @@
     Dim cima As Boolean = False
     Dim esquerda As Boolean = False
     Dim direita As Boolean = False
+    Dim ganhar As Boolean = True
+
 
     Public Sub New()
 
@@ -24,13 +26,24 @@
     End Sub
 
     Private Sub Button1_MouseHover(sender As Object, e As EventArgs) Handles Button1.MouseHover
-        Mundo1.gamprogress.Increment(25)
-        MsgBox("Conseguiste!! :D", MsgBoxStyle.Information, "Parabéns")
-        Me.Hide()
-        Mundo1.Show()
+        If ganhar = True Then
+            Mundo1.gamprogress.Increment(25)
+            MsgBox("Conseguiste!! :D", MsgBoxStyle.Information, "Parabéns")
+            Me.Hide()
+            Mundo1.Show()
+        Else
+            MsgBox("Não faças batota, tenta outra vez.", MsgBoxStyle.Information, "):")
+            MoveToStart()
+            ganhar = True
+        End If
     End Sub
 
     Private Sub Wall_MouseEnter(sender As Object, e As EventArgs) Handles Label9.MouseEnter, Label8.MouseEnter, Label7.MouseEnter, Label6.MouseEnter, Label5.MouseEnter, Label4.MouseEnter, Label3.MouseEnter, Label20.MouseEnter, Label2.MouseEnter, Label19.MouseEnter, Label18.MouseEnter, Label17.MouseEnter, Label16.MouseEnter, Label15.MouseEnter, Label14.MouseEnter, Label13.MouseEnter, Label12.MouseEnter, Label11.MouseEnter, Label10.MouseEnter
         MoveToStart()
+    End Sub
+
+    Private Sub Pok_Man_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ganhar = False
+
     End Sub
 End Class
